62683fcda7b68ef143cc6a9190e384ae3ad79787
muttaqinrizal <rizalm57@gmail.com>
2023-01-18 19:15:13 +0700

    feat : dependencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    baseUrl: "https://dummyjson.com/",
+    allProduct: 100,
+    baseLimit: 10,
+    cartDetailProductsLimit: 5,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.dummyjson.com',
+        port: '',
+        pathname: '/data/**',
+      },
+    ],
+  },
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/products",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
